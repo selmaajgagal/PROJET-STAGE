ea2a745065c5ba9b0a88b3210b9256d478305a53
selmaajgagal <159468839+selmaajgagal@users.noreply.github.com>
2024-12-03 20:14:57 +0100

    Delete setup_db.py

--- setup_db.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from models import Base
-
-
-engine = create_engine('sqlite:///feedback.db')  
-Base.metadata.create_all(engine)  
-
-print("Database and tables created successfully.")
